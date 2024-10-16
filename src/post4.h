SirWumpus/post4

commit 656e08f349d8afad6611008e65652b222e58d012

GH-50 Fix CI (Ubuntu) header order to address sigjmp_buf errors.

src/post4.h
@@ -29,9 +29,6 @@ extern "C" {
 # define ECHOCTL			0
 #endif
 
-#include "config.h"
-#include "ansiterm.h"
-
 #ifndef P4_MEM_SIZE
 /* When selecting the default data space memory size, consider the number
  * of cells that will be available for defining the core and new words.
@@ -136,6 +133,8 @@ extern "C" {
 #include <signal.h>
 #include <time.h>
 
+#include "config.h"
+#include "ansiterm.h"
 
 #ifdef HAVE_ERR_H
 # include <err.h>

commit aa8555f9c33081867be94538a34f770b75b8dbb7

Fix broken SH and SHELL words caused by commit 656e08f349.

src/post4.h
@@ -30,6 +30,8 @@ extern "C" {
 # define ECHOCTL			0
 #endif
 
+#include "config.h"
+
 #ifndef P4_MEM_SIZE
 /* When selecting the default data space memory size, consider the number
  * of cells that will be available for defining the core and new words.
@@ -130,7 +132,6 @@ extern "C" {
 #include <signal.h>
 #include <time.h>
 
-#include "config.h"
 #include "ansiterm.h"
 
 #ifdef HAVE_ERR_H